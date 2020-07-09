706349ad61f186ffb7f66e04e24bfeb1acc47643
Alex Naish <alex@naish.io>
2020-07-09 14:31:12 +0100

    fix: switch to build end to allow for HTML to be fully updated first

--- index.js
+++ index.js
@@ -17,48 +17,47 @@ const defaultOptions = {
 global.location = { pathname: '/' };
 
 module.exports = async (bundler) => {
-
   // If you're in watch mode - you're developing so skip
   if (bundler.options.watch) {
     return;
   }
 
-  bundler.on('bundled', async ({ name, entryAsset }) => {
 
-    // Merge package.json pregenerate config block with default options
+  bundler.on('buildEnd', async () => {
+    // Merge package.json pregenerate config with default options
+    const { name, entryAsset } = bundler.mainBundle;
     const { pregenerate = {} } = await entryAsset.getPackage();
     const options = Object.assign({}, defaultOptions, pregenerate);
     const { rootDir } = bundler.options;
 
-    // If the file just bundled was our template file - proceed
-    if (name.includes(options.templateFile)) {
+    // If this build wasn't for the template - skip over it
+    if (!name.includes(options.templateFile)) {
+      return;
+    }
 
-      // Bundle and transform the file to generate
-      const newBundler = new Bundler(path.join(rootDir, options.entryFile), { target: 'node', sourceMaps: false, logLevel: 2 });
-      const result = await newBundler.bundle();
+    // Bundle and transform the file to generate
+    const newBundler = new Bundler(path.join(rootDir, options.entryFile), { target: 'node', sourceMaps: false, logLevel: 2 });
+    const result = await newBundler.bundle();
 
-      // Render the resulting file to static HTML
-      const Static = require(result.name)[options.exportedName];
-      const markup = renderToStaticMarkup(React.createElement(Static, {}, null));
+    // Render the resulting file to static HTML
+    const Static = require(result.name)[options.exportedName];
+    const markup = renderToStaticMarkup(React.createElement(Static, {}, null));
 
-      // Update HTML template
-      const $ = cheerio.load(entryAsset.generated.html);
-      $(options.rootElement).html(markup);
-      entryAsset.generated.html = $.html();
+    // Update HTML template
+    const existingHtml = fs.readFileSync(name);
+    const $ = cheerio.load(existingHtml);
+    $(options.rootElement).html(markup);
+    entryAsset.generated.html = $.html();
+    fs.writeFileSync(name, $.html());
 
-      // Clean up temporary files
-      if (options.removeFilesOnCompletion) {
-        const filesToDelete = new Set([
-          result.name,
-          ...Array.from(result.childBundles).map(bundle => bundle.name),
-          ...Array.from(result.siblingBundles).map(bundle => bundle.name),
-        ]);
-        filesToDelete.forEach(filePath => fs.unlinkSync(filePath))
-      }
-
-      // Update the HTML file
-      fs.writeFileSync(name, entryAsset.generated.html);
-
+    // Clean up temporary files
+    if (options.removeFilesOnCompletion) {
+      const filesToDelete = new Set([
+        result.name,
+        ...Array.from(result.childBundles).map(bundle => bundle.name),
+        ...Array.from(result.siblingBundles).map(bundle => bundle.name),
+      ]);
+      filesToDelete.forEach(filePath => fs.unlinkSync(filePath))
     }
 
   });
